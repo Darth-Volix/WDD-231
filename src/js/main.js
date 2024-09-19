d994aaae81111f323828f0692be35d90aa9e21f5
Nicholas Wilkins <150397998+Darth-Volix@users.noreply.github.com>
2024-09-19 10:42:54 -0600

    started on JS for NPS 1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+// Add the park name and link to the disclaimer
+let disclaimer = document.querySelector(".disclaimer a");
+
+disclaimer.innerHTML = parkData.fullName;
+disclaimer.href = parkData.url;
