e8d1bc0f5cf4f4b0cb14be44fc8cf32c73da9004
Nicholas Wilkins <150397998+Darth-Volix@users.noreply.github.com>
2024-09-19 11:12:40 -0600

    Continued work on JS for NPS1

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,30 @@ import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
 
+// This code will use this template to display the park info
+function parkInfoTemplate(info) {
+    return `<a href="/" class="hero-banner__title">${info.name}</a>
+    <p class="hero-banner__subtitle">
+      <span>${info.designation}</span>
+      <span>${info.states}</span>
+    </p>`;
+  }
+
+// Add the park name to the title of the webpage
+let title = document.querySelector("title");
+
+title.innerHTML = parkData.fullName;
+
 // Add the park name and link to the disclaimer
 let disclaimer = document.querySelector(".disclaimer a");
 
 disclaimer.innerHTML = parkData.fullName;
 disclaimer.href = parkData.url;
+
+// Add the hero banner image
+let heroBannerImage = document.querySelector('.hero-banner img');
+
+heroBannerImage.src = parkData.images[0].url;
+heroBannerImage.alt = parkData.images[0].altText;
+
+// COME BACK TO THIS LATER
