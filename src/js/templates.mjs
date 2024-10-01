0aecf8ca92f5ec69dc09d55d3422d5ec3804f3b9
Nicholas Wilkins <150397998+Darth-Volix@users.noreply.github.com>
2024-10-01 10:16:55 -0600

    Refactor parkInfoTemplate, parkFooterTemplate, and mediaCardTemplate to improve code readability and formatting

--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -1,36 +1,36 @@
 export function parkInfoTemplate(info) {
-    // add the park name, designation, and states to the hero
-    return `<a href="/" class="hero-banner__title">${info.name}</a>
-    <p class="hero-banner__subtitle">
-      <span>${info.designation}</span>
-      <span>${info.states}</span>
-    </p>`;
+  // add the park name, designation, and states to the hero
+  return `<a href="/" class="hero-banner__title">${info.name}</a>
+  <p class="hero-banner__subtitle">
+    <span>${info.designation}</span>
+    <span>${info.states}</span>
+  </p>`;
 }
 
 export function parkFooterTemplate(info) {
-    // add the park phone number to the footer
-    const voice = getVoicePhone(info.contacts.phoneNumbers);
-    return `
-    <h3>Contact Info</h3>
-    <h4>Phone:</h4>
-    <p>${voice}</p>
-    `
+  // add the park phone number to the footer
+  const voice = getVoicePhone(info.contacts.phoneNumbers);
+  return `
+  <h3>Contact Info</h3>
+  <h4>Phone:</h4>
+  <p>${voice}</p>
+  `
 }
 
 function getVoicePhone(numbers) {
-    // find the phone number with the type "Voice" (think of this like a "where" clause in SQL)
-    const voice = numbers.find((number) => number.type === "Voice");
-    return voice.phoneNumber;
+  // find the phone number with the type "Voice" (think of this like a "where" clause in SQL)
+  const voice = numbers.find((number) => number.type === "Voice");
+  return voice.phoneNumber;
 }
 
 export function mediaCardTemplate(info) {
-    // add the image, name, and description to the media card
-    return `
-    <div class="media-card">
-      <a href="${info.link}">
-        <img src="${info.image}" alt="${info.name}">
-        <h3>${info.name}</h3>
-      </a>
-      <p>${info.description}</p>
-    </div>`
+  // add the image, name, and description to the media card
+  return `
+  <div class="media-card">
+    <a href="${info.link}">
+      <img src="${info.image}" alt="${info.name}">
+      <h3>${info.name}</h3>
+    </a>
+    <p>${info.description}</p>
+  </div>`
   }
