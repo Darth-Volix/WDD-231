66b71fae56fb02c23b3d6cf7d387d3a7b74ffdd8
Nicholas Wilkins <150397998+Darth-Volix@users.noreply.github.com>
2024-10-17 10:50:20 -0600

    Refactor alertTemplate function to handle "Park Closure" category separately

--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -38,13 +38,13 @@ export function mediaCardTemplate(info) {
 export function alertTemplate(alert) {
   let alertType = "";
   // most of the alerts are one word and line up with the icons nicely. "Park Closure" is the exception
-  switch (alert.category) {
-    case "Park Closure":
-      alertType = "closure";
-      break;
-    default:
-      alertType = alert.category.toLowerCase();
+  
+  if (alert.category === "Park Closure") {
+    alertType = "closure";
+  } else {
+    alertType = alert.category.toLowerCase();
   }
+  
   return `<li class="alert">
   <svg class="icon" focusable="false" aria-hidden="true"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="/images/sprite.symbol.svg#alert-${alertType}"></use></svg>
   <div>
