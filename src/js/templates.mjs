676bffb2d3698639a039958473cb522c8f09651a
Nicholas Wilkins <150397998+Darth-Volix@users.noreply.github.com>
2024-10-19 11:43:55 -0600

    Refactor SVG path in alertTemplate function and update SVG sprite path

--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -1,3 +1,5 @@
+import spritePath from "../images/sprite.symbol.svg";
+
 export function parkInfoTemplate(info) {
   // add the park name, designation, and states to the hero
   return `<a href="/" class="hero-banner__title">${info.name}</a>
@@ -46,7 +48,9 @@ export function alertTemplate(alert) {
   }
   
   return `<li class="alert">
-  <svg class="icon" focusable="false" aria-hidden="true"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="images/sprite.symbol.svg#alert-${alertType}"></use></svg>
+  <svg class="icon" focusable="false" aria-hidden="true">
+       <use href="${spritePath}#alert-${alertType}"></use>
+  </svg>
   <div>
     <h3 class="alert-${alertType}">${alert.title}</h3>
     <p>${alert.description}</p>
